cc066e70a44019aa0a4c795386878c0ccf249891
yariv <yariv@ptbox.io>
2023-01-24 11:10:47 +0200

    Added network creation for controller module

--- initialize_script.sh
+++ initialize_script.sh
@@ -1,7 +1,9 @@
+# Creation the modules networks
 docker network create --driver bridge analyze_module_network
 docker network create --driver bridge password_module_network
+docker network create --driver bridge controller_module_network
 
-
+# Building the modules images
 docker build -f analyze_module/Dockerfile . -t analyze_module
 docker build -f password_module/Dockerfile . -t password_module
 docker build -f controller_module/Dockerfile . -t controller_module
